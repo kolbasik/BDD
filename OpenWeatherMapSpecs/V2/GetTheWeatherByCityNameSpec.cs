35c924529442742c8bf97ba3dfd5e3a6f4ebbced
kolbasik <sergei.kolbasin@gmail.com>
2016-05-28 23:40:43 +0200

    changed the test style

--- OpenWeatherMapSpecs/V2/GetTheWeatherByCityNameSpec.cs
+++ OpenWeatherMapSpecs/V2/GetTheWeatherByCityNameSpec.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NBDD.V2;
 using OpenWeatherMapSpecs.V2.Specs;
 using Xunit;
@@ -29,14 +28,13 @@ namespace OpenWeatherMapSpecs.V2
                 {
                     the.Scenario()
                         .Use<TheWeatherByCityNameSpec>()
-                            .Given($"the city is {cityName}", x => x.Сity_is_cityName(cityName))
-                                .And($"the country is {countryCode}", x => x.Сountry_is_countryName(countryCode))
+                            .Given($"the city is {cityName}", x => x.CityName = cityName)
+                                .And($"the country is {countryCode}", x => x.CountryCode = countryCode)
                             .When(@"I have asked the forecast", x => x.When_I_have_asked_the_forecast())
-                            .Then(@"I will get the wheather forecast", x => x.Then_I_will_get_the_wheather_forecast())
-                                .And(@"the forecast should contain the city name", x => x.The_forecast_should_contain_the_city_name())
-                                .And(@"the forecast should contain the coordinates", x => x.The_forecast_should_contain_the_coordinates())
-                                .And(@"the forecast should contain the main information", x => x.The_forecast_should_contain_the_main_information())
-                                .And(@"the forecast should contain the wheather information", x => x.The_forecast_should_contain_the_wheather_information());
+                            .Then(@"it should contain the city name", x => x.It_should_contain_the_city_name())
+                                .And(@"it should contain the coordinates", x => x.It_should_contain_the_coordinates())
+                                .And(@"it should contain the main information", x => x.It_should_contain_the_main_information())
+                                .And(@"it should contain the wheather information", x => x.It_should_contain_the_wheather_information());
                 })
                 .PlayAsync(_output.WriteLine);
         }
--- OpenWeatherMapSpecs/V2/Specs/TheWeatherByCityNameSpec.cs
+++ OpenWeatherMapSpecs/V2/Specs/TheWeatherByCityNameSpec.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenWeatherMap;
 
@@ -7,48 +6,32 @@ namespace OpenWeatherMapSpecs.V2.Specs
 {
     public sealed class TheWeatherByCityNameSpec : OpenWeatherMapServiceSpec
     {
-        public string CityName { get; private set; }
-        public string CountryName { get; private set; }
+        public string CityName { get; set; }
+        public string CountryCode { get; set; }
         public OpenWeatherMapResult Result { get; private set; }
-
-        public void Сity_is_cityName(string cityName)
-        {
-            if (cityName == null) throw new ArgumentNullException("cityName");
-            CityName = cityName;
-        }
-
-        public void Сountry_is_countryName(string countryName)
-        {
-            if (countryName == null) throw new ArgumentNullException("countryName");
-            CountryName = countryName;
-        }
 
         public async Task When_I_have_asked_the_forecast()
         {
-            Result = await OpenWeatherMapService.GetWeatherForecastAsync(CityName, CountryName).ConfigureAwait(false);
-        }
-
-        public void Then_I_will_get_the_wheather_forecast()
-        {
+            Result = await OpenWeatherMapService.GetWeatherForecastAsync(CityName, CountryCode).ConfigureAwait(false);
             Assert.IsNotNull(Result);
         }
 
-        public void The_forecast_should_contain_the_city_name()
+        public void It_should_contain_the_city_name()
         {
             Assert.AreEqual(CityName, Result.Name);
         }
 
-        public void The_forecast_should_contain_the_coordinates()
+        public void It_should_contain_the_coordinates()
         {
             Assert.IsNotNull(Result.Coord);
         }
 
-        public void The_forecast_should_contain_the_main_information()
+        public void It_should_contain_the_main_information()
         {
             Assert.IsNotNull(Result.Main);
         }
 
-        public void The_forecast_should_contain_the_wheather_information()
+        public void It_should_contain_the_wheather_information()
         {
             Assert.IsNotNull(Result.Weather);
         }
