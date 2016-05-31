5158e4a34e536853af756f69409df1a6571438da
kolbasik <sergei.kolbasin@gmail.com>
2016-05-31 13:27:19 +0200

    changed AsAsync implementation

--- NBDD/V2/F.cs
+++ NBDD/V2/F.cs
@@ -8,58 +8,63 @@ namespace NBDD.V2
     public static class F
     {
         [DebuggerHidden]
-        public static Func<TArg1, TArg2, Task> AsAsync<TArg1, TArg2>(this Action<TArg1, TArg2> action)
+        public static Lazy<TReturn> Lazy<TReturn>(TReturn value) => new Lazy<TReturn>(Identity(value));
+
+        [DebuggerHidden]
+        public static Func<TReturn> Identity<TReturn>(TReturn value) => () => value;
+
+        [DebuggerHidden]
+        public static Func<T1, T2, Task> AsAsync<T1, T2>(this Action<T1, T2> action)
         {
-            return (arg1, arg2) => action.Bind(arg1).Bind(arg2).AsAsync();
+            return (arg1, arg2) => action.Bind(arg1).Bind(arg2).AsAsync().Invoke();
         }
 
         [DebuggerHidden]
-        public static Func<TArg1, Task> AsAsync<TArg1>(this Action<TArg1> action)
+        public static Func<T1, Task> AsAsync<T1>(this Action<T1> action)
         {
-            return arg1 => action.Bind(arg1).AsAsync();
+            return arg1 => action.Bind(arg1).AsAsync().Invoke();
         }
 
         [DebuggerHidden]
-        public static async Task AsAsync(this Action action)
+        public static Func<Task> AsAsync(this Action action)
         {
-            await Task.Yield();
-            action();
+            return () => Task.Factory.FromAsync(action.BeginInvoke, action.EndInvoke, null);
         }
 
         [DebuggerHidden]
-        public static Action Bind<TArg>(this Action<TArg> action, TArg arg)
+        public static Action Bind<T>(this Action<T> action, T arg)
         {
-            return () => action(arg);
+            return () => action.Invoke(arg);
         }
 
         [DebuggerHidden]
-        public static Action<TArg2> Bind<TArg1, TArg2>(this Action<TArg1, TArg2> action, TArg1 arg1)
+        public static Action<T2> Bind<T1, T2>(this Action<T1, T2> action, T1 arg1)
         {
-            return arg2 => action(arg1, arg2);
+            return arg2 => action.Invoke(arg1, arg2);
         }
 
         [DebuggerHidden]
-        public static Func<TReturn> Bind<TArg, TReturn>(this Func<TArg, TReturn> func, TArg arg)
+        public static Func<TReturn> Bind<T, TReturn>(this Func<T, TReturn> func, T arg)
         {
-            return () => func(arg);
+            return () => func.Invoke(arg);
         }
 
         [DebuggerHidden]
-        public static Func<TReturn> Bind<TArg, TReturn>(this Func<TArg, TReturn> func, Lazy<TArg> arg)
+        public static Func<TReturn> Bind<T, TReturn>(this Func<T, TReturn> func, Lazy<T> arg)
         {
-            return () => func(arg.Value);
+            return () => func.Invoke(arg.Value);
         }
 
         [DebuggerHidden]
-        public static Func<TArg2, TReturn> Bind<TArg1, TArg2, TReturn>(this Func<TArg1, TArg2, TReturn> func, TArg1 arg1)
+        public static Func<T2, TReturn> Bind<T1, T2, TReturn>(this Func<T1, T2, TReturn> func, T1 arg1)
         {
-            return arg2 => func(arg1, arg2);
+            return arg2 => func.Invoke(arg1, arg2);
         }
 
         [DebuggerHidden]
-        public static Func<TArg2, TReturn> Bind<TArg1, TArg2, TReturn>(this Func<TArg1, TArg2, TReturn> func, Lazy<TArg1> arg1)
+        public static Func<T2, TReturn> Bind<T1, T2, TReturn>(this Func<T1, T2, TReturn> func, Lazy<T1> arg1)
         {
-            return arg2 => func(arg1.Value, arg2);
+            return arg2 => func.Invoke(arg1.Value, arg2);
         }
     }
 }
